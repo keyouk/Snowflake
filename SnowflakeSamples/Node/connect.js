8e5c58f3029b142bd3bb40c5aa3ffe70cd6af7ac
KeYouk <kelvin.youk@gmail.com>
2021-06-01 20:21:04 -0700

    removed syntax error

--- SnowflakeSamples/Node/connect.js
+++ SnowflakeSamples/Node/connect.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const snowflake = require('snowflake-sdk');
-const PORT = 3002;
 
 snowflake.configure({ logLevel: 'trace' });
 
